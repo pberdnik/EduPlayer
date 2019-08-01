9195764f37aa5054aef22db84fb304dbefc13de6
bpa95 <berpaval@gmail.com>
2019-08-01 13:29:52 +0300

    feat: Enable data binding

--- app/src/main/java/io/github/pberdnik/eduplayer/demo/DemoFragment.kt
+++ app/src/main/java/io/github/pberdnik/eduplayer/demo/DemoFragment.kt
@@ -6,19 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import io.github.pberdnik.eduplayer.R
+import io.github.pberdnik.eduplayer.databinding.DemoFragmentBinding
 
 class DemoFragment : Fragment() {
 
-    private lateinit var viewModel: DemoViewModel
+    private val viewModel by lazy { ViewModelProviders.of(this).get(DemoViewModel::class.java) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.demo_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(DemoViewModel::class.java)
+        val binding = DemoFragmentBinding.inflate(inflater)
+        return binding.root
     }
 
 }
