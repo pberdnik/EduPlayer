7e14d189f4be517d207fe534c4db8cb1f70f4261
pberdnik <berpaval@gmail.com>
2019-08-16 15:39:08 +0300

    feat: Configure retrofit

---
+++ app/src/main/java/io/github/pberdnik/eduplayer/network/RetrofitConfig.kt
@@ -0,0 +1,46 @@
+package io.github.pberdnik.eduplayer.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.github.pberdnik.eduplayer.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+private const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
+
+
+// add apiKey query parameter to each request
+private val apiKeyInterceptor = { chain: Interceptor.Chain ->
+    val url = chain.request().url.newBuilder()
+        .addQueryParameter("key", BuildConfig.YouTubeApiKey)
+        .build()
+    val request = chain.request().newBuilder()
+        .url(url)
+        .build()
+    chain.proceed(request)
+}
+
+private val loggingInterceptor = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.BODY
+}
+
+private val okHttpClient = OkHttpClient().newBuilder()
+    .addInterceptor(loggingInterceptor)
+    .addInterceptor(apiKeyInterceptor)
+    .build()
+
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+
+val retrofit: Retrofit = Retrofit.Builder()
+    .client(okHttpClient)
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
