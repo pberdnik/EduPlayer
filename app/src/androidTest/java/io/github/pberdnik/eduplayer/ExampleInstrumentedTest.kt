efb41b0bf96509570b1105655ce402028afccc34
pberdnik <berpaval@gmail.com>
2019-08-31 16:59:06 +0300

    test: Add database testing playground

--- app/src/androidTest/java/io/github/pberdnik/eduplayer/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/github/pberdnik/eduplayer/ExampleInstrumentedTest.kt
@@ -1,8 +1,14 @@
 package io.github.pberdnik.eduplayer
 
+import androidx.room.Room
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-import org.junit.Assert.assertEquals
+import io.github.pberdnik.eduplayer.database.PlaylistDao
+import io.github.pberdnik.eduplayer.database.YoutubeDatabase
+import io.github.pberdnik.eduplayer.repository.YoutubeRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,10 +19,34 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
+
+    private lateinit var database: YoutubeDatabase
+    private lateinit var playlistDao: PlaylistDao
+    private lateinit var repository: YoutubeRepository
+
+    @Before
+    fun createDb() {
+
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("io.github.pberdnik.eduplayer", appContext.packageName)
+        database = Room.inMemoryDatabaseBuilder(appContext, YoutubeDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        playlistDao = database.playlistDao
+        repository = YoutubeRepository(database)
+    }
+
+    @After
+    fun closeDb() {
+        database.close()
+    }
+
+    @Test
+    fun test() = runBlocking {
+        repository.refreshPlaylists()
+        repository.switchPlaylistExpansion("PLoFRvPlmME6E_nr_ulTL1-qO_-Y4I-hmk")
+        println("EXPANSION:" + playlistDao.getExpandedPlaylistsItemsNoLD())
+        repository.switchPlaylistExpansion("PLoFRvPlmME6E_nr_ulTL1-qO_-Y4I-hmk")
+        println("EXPANSION:" + playlistDao.getExpandedPlaylistsItemsNoLD())
     }
 }
