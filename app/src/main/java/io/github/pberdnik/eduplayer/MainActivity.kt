28b70b183f9e43830851edd0bc3c034a04ab4dfd
pberdnik <berpaval@gmail.com>
2019-09-02 19:01:54 +0300

    feat: Add logo and appbar

--- app/src/main/java/io/github/pberdnik/eduplayer/MainActivity.kt
+++ app/src/main/java/io/github/pberdnik/eduplayer/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(app_bar)
+        supportActionBar?.setIcon(R.drawable.ic_edu_logo)
 
         val navController = findNavController(R.id.mainNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
