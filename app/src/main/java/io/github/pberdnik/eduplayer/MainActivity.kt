a663a7cb1022b3c6631a76ffa112a6b00cebea1b
pberdnik <berpaval@gmail.com>
2019-09-14 15:07:56 +0300

    feat: Implement bottom navigation as in advanced google sample

--- app/src/main/java/io/github/pberdnik/eduplayer/MainActivity.kt
+++ app/src/main/java/io/github/pberdnik/eduplayer/MainActivity.kt
@@ -2,16 +2,63 @@ package io.github.pberdnik.eduplayer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * An activity that inflates a layout that has a [BottomNavigationView].
+ */
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottom_navigation.setupWithNavController(findNavController(R.id.nav_host_fragment))
+        setSupportActionBar(fake_toolbar)
+
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        } // Else, need to wait for onRestoreInstanceState
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        val navGraphIds = listOf(R.navigation.learn, R.navigation.explore, R.navigation.library)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
+
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
--- app/src/main/java/io/github/pberdnik/eduplayer/features/explore/ExploreFragment.kt
+++ app/src/main/java/io/github/pberdnik/eduplayer/features/explore/ExploreFragment.kt
@@ -1,14 +1,14 @@
 package io.github.pberdnik.eduplayer.features.explore
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.onNavDestinationSelected
 import io.github.pberdnik.eduplayer.R
 import io.github.pberdnik.eduplayer.databinding.ExploreFragmentBinding
 import io.github.pberdnik.eduplayer.features.explore.playlistrecyclerview.PlaylistAdapter
@@ -40,7 +40,7 @@ class ExploreFragment : Fragment() {
                 findNavController().navigate(
                     ExploreFragmentDirections.actionShowDetails(playlistId)
                 )
-                // Tell the ViewModel we've made the navigate call to prevent multiple navigation
+                // Tell the ViewModel we've made the navigate call to prevent multiple account
                 viewModel.displayPlaylistDetailsComplete()
             }
         })
@@ -50,18 +50,18 @@ class ExploreFragment : Fragment() {
             supportActionBar?.setIcon(R.drawable.ic_edu_logo_with_padding)
         }
 
-        setHasOptionsMenu(true)
+//        setHasOptionsMenu(true)
 
         return binding.root
     }
 
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_main, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) =
-        item.onNavDestinationSelected(view!!.findNavController())
-                || super.onOptionsItemSelected(item)
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+//        inflater.inflate(R.menu.menu_main, menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem) =
+//        item.onNavDestinationSelected(view!!.findNavController())
+//                || super.onOptionsItemSelected(item)
 }
