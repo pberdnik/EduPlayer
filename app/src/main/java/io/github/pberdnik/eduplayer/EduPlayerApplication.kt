b50a43f66c110b74910047fcab77c1af4329c95e
pberdnik <berpaval@gmail.com>
2019-09-30 11:36:33 +0300

    feat: Add Timber logging lib

--- app/src/main/java/io/github/pberdnik/eduplayer/EduPlayerApplication.kt
+++ app/src/main/java/io/github/pberdnik/eduplayer/EduPlayerApplication.kt
@@ -3,11 +3,22 @@ package io.github.pberdnik.eduplayer
 import android.app.Application
 import io.github.pberdnik.eduplayer.di.DaggerAppComponent
 import io.github.pberdnik.eduplayer.di.InjectorProvider
+import timber.log.Timber
 
 class EduPlayerApplication : Application(), InjectorProvider {
 
     override val component by lazy {
         DaggerAppComponent.factory().create(applicationContext)
     }
+
+    override fun onCreate() {
+        super.onCreate()
+        setupLogger()
+    }
+
+    private fun setupLogger() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
 
 }
