2d319801e9b1709ce37784c11bd0d2b72c44f0b8
pberdnik <berpaval@gmail.com>
2019-10-11 02:25:22 +0300

    feat: Display device video thumbnail

--- app/src/main/java/io/github/pberdnik/eduplayer/features/library/devicevideos/BindingAdapters.kt
+++ app/src/main/java/io/github/pberdnik/eduplayer/features/library/devicevideos/BindingAdapters.kt
@@ -1,10 +1,23 @@
 package io.github.pberdnik.eduplayer.features.library.devicevideos
 
+import android.net.Uri
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import io.github.pberdnik.eduplayer.domain.DeviceVideo
 
 @BindingAdapter("listData")
 fun RecyclerView.bindListData(data: List<DeviceVideo>?) {
     (adapter as DeviceVideoAdapter).submitList(data)
+}
+
+@BindingAdapter("videoUri")
+fun ImageView.bindImage(videoUri: String?) {
+    videoUri?.let {
+        Glide.with(context)
+            .asBitmap()
+            .load(Uri.parse(videoUri))
+            .into(this)
+    }
 }
