32a145c5d4ed2ea5a5c3932d3d95b5446347afe3
pberdnik <berpaval@gmail.com>
2019-10-11 20:59:58 +0300

    feat: Display device video duration

--- app/src/main/java/io/github/pberdnik/eduplayer/repository/LocalRepository.kt
+++ app/src/main/java/io/github/pberdnik/eduplayer/repository/LocalRepository.kt
@@ -1,7 +1,9 @@
 package io.github.pberdnik.eduplayer.repository
 
 import android.content.ContentResolver
+import android.content.Context
 import android.database.Cursor
+import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.provider.OpenableColumns
 import dagger.Reusable
@@ -11,30 +13,45 @@ import javax.inject.Inject
 
 @Reusable
 class LocalRepository @Inject constructor(
-    val localDao: LocalDao,
-    val contentResolver: ContentResolver
+    private val localDao: LocalDao,
+    private val contentResolver: ContentResolver,
+    private val context: Context
 ) {
     val deviceVideos = localDao.getVideos()
 
     suspend fun insertVideos(uris: List<String>) {
-        val dbUris = uris.map { uri ->
-            val (title, duration) = dumpVideoMetaData(Uri.parse(uri))
-            DatabaseDeviceVideo(uri = uri, title = title, duration = duration)
-        }
+        val dbUris = uris.map { uri -> getDeviceVideoWithMetaData(Uri.parse(uri)) }
         localDao.insertVideos(*dbUris.toTypedArray())
     }
 
-    fun dumpVideoMetaData(uri: Uri): Pair<String?, String?> {
+    private fun getDeviceVideoWithMetaData(uri: Uri): DatabaseDeviceVideo {
+        val retriever = MediaMetadataRetriever()
+        retriever.setDataSource(context, uri)
+        val duration = convertToHumanDuration(
+            retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION).toLong()
+        )
+        retriever.release()
+
         var displayName: String? = null
-        var size: String? = null
         val cursor: Cursor? = contentResolver.query(uri, null, null, null, null, null)
         cursor?.use {
             if (it.moveToFirst()) {
                 displayName = it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
-                val sizeIndex: Int = it.getColumnIndex(OpenableColumns.SIZE)
-                size = if (!it.isNull(sizeIndex)) it.getString(sizeIndex) else "Unknown"
             }
         }
-        return displayName to size
+
+        return DatabaseDeviceVideo(uri = uri.toString(), title = displayName, duration = duration)
+    }
+
+    private fun convertToHumanDuration(durationMillis: Long): String {
+        val durationSeconds = durationMillis / 1000
+        val h = durationSeconds / 60 / 60
+        val m = durationSeconds % (60 * 60) / 60
+        val s = durationSeconds % 60
+        return if (h == 0L) {
+            String.format("%d:%02d", m, s)
+        } else {
+            String.format("%d:%02d:%02d", h, m, s)
+        }
     }
 }
